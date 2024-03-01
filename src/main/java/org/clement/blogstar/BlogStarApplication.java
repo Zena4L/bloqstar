15b928bca0256533b28d20a74eaf49aa0b72babb
Clement Owireku-Bogyah <clement.owireku-bogyah@amalitech.com>
2024-03-01 09:47:06 +0000

    first commit

---
+++ src/main/java/org/clement/blogstar/BlogStarApplication.java
@@ -0,0 +1,13 @@
+package org.clement.blogstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogStarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogStarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/clement/blogstar/BlogStarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.clement.blogstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogStarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
